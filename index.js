3c374c65ec17e723e4e5cca8b43ee8900c31843e
Luchanskiy Oleg <luchansodev@gmail.com>
2018-09-01 12:06:21 +0300

    add ideal calculation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const YF = require('yahoo-finance');
 const fs = require('fs');
 
 const dataString = fs.readFileSync('./symbols.txt', { encoding: 'utf-8' });
-const symbols = dataString.split(',');
+const symbols = dataString.split(',').map(item => item.trim());
 
 const modules = ['price', 'summaryDetail', 'defaultKeyStatistics'];
 
@@ -21,7 +21,16 @@ const start = async () => {
         const margin = (defaultKeyStatistics.profitMargins * 100).toFixed(3);
         const PE = summaryDetail.trailingPE.toFixed(3);
         const { symbol: symbolKey } = price;
+        const PEMargin = ((1 / summaryDetail.trailingPE) * defaultKeyStatistics.profitMargins).toFixed(3);
 
-        console.log(`symbol: ${symbolKey}, margin: ${margin}, P/E: ${PE}`);
+        console.log(`symbol = ${symbolKey}, margin = ${margin}, P/E = ${PE}, (1 / (P/E)) * margin = ${PEMargin}`);
     }
 }
+
+const idealPE = 72 / 6;
+const idealMargin = 0.2;
+const idealPEMargin = ((1 / idealPE) * idealMargin).toFixed(3);
+
+console.log(`Ideal: (1 / (P/E)) * margin = ${idealPEMargin}`);
+
+start();
